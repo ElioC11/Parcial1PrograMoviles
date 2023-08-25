ec20af5ae3b4af72d99562f0647c6ad2062bed06
ElioC11 <eliocifuentes952@gmail.com>
2023-08-25 01:10:54 -0600

    commit 2

--- ejemplo1/lib/main.dart
+++ ejemplo1/lib/main.dart
@@ -1,5 +1,4 @@
 //import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,24 +6,29 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primaryColor: const Color(0x0036435D),
+        textTheme: TextTheme(
+          bodyText1:
+              TextStyle(color: Colors.white), // Cambia el color del texto aquí
+          bodyText2:
+              TextStyle(color: Colors.white), // Cambia el color del texto aquí
+          // Agrega más estilos de texto según sea necesario
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Torneo Futbol 7 Mesoamericana'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -34,153 +38,213 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
-  Widget build(BuildContext context) => Scaffold(
-        body: DataTable(
-          columns: const [
-            DataColumn(label: Text('Nombre')),
-            DataColumn(label: Text('PTS')),
-            DataColumn(label: Text('PJ')),
-            DataColumn(label: Text('PG')),
-            DataColumn(label: Text('PE')),
-            DataColumn(label: Text('PP')),
-            DataColumn(label: Text('GF')),
-            DataColumn(label: Text('GC')),
-            DataColumn(label: Text('DIF')),
-          ],
-          rows: const [
-            DataRow(cells: [
-              DataCell(Text('Pythonicos')),
-              DataCell(Text('9')),
-              DataCell(Text('3')),
-              DataCell(Text('3')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('8')),
-              DataCell(Text('0')),
-              DataCell(Text('+8')),
-            ]),
-            DataRow(cells: [
-              DataCell(Text('Equipo 2')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-            ]),
-            DataRow(cells: [
-              DataCell(Text('Equipo 3')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-            ]),
-            DataRow(cells: [
-              DataCell(Text('Equipo 4')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-            ]),
-            DataRow(cells: [
-              DataCell(Text('Equipo 5')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-            ]),
-            DataRow(cells: [
-              DataCell(Text('Equipo 6')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-            ]),
-            DataRow(cells: [
-              DataCell(Text('Equipo 7')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-            ]),
-            DataRow(cells: [
-              DataCell(Text('Equipo 8')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-            ]),
-            DataRow(cells: [
-              DataCell(Text('Equipo 9')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-            ]),
-            DataRow(cells: [
-              DataCell(Text('Equipo 10')),
-              DataCell(Text('0')),
-              DataCell(Text('1')),
-              DataCell(Text('1')),
-              DataCell(Text('0')),
-              DataCell(Text('1')),
-              DataCell(Text('0')),
-              DataCell(Text('1')),
-              DataCell(Text('-1')),
-            ]),
-            DataRow(cells: [
-              DataCell(Text('Equipo 11')),
-              DataCell(Text('0')),
-              DataCell(Text('1')),
-              DataCell(Text('1')),
-              DataCell(Text('0')),
-              DataCell(Text('1')),
-              DataCell(Text('0')),
-              DataCell(Text('3')),
-              DataCell(Text('-3')),
-            ]),
-            DataRow(cells: [
-              DataCell(Text('Equipo 12')),
-              DataCell(Text('0')),
-              DataCell(Text('1')),
-              DataCell(Text('0')),
-              DataCell(Text('0')),
-              DataCell(Text('1')),
-              DataCell(Text('0')),
-              DataCell(Text('4')),
-              DataCell(Text('-4')),
-            ]),
-          ],
-        ),
-      );
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      backgroundColor: const Color(0xFF3C63B2),
+      body: DataTable(
+        headingRowColor:
+            MaterialStateColor.resolveWith((states) => Colors.grey[800]!),
+        columns: const [
+          DataColumn(
+            label: Text('Nombre',
+                style: TextStyle(
+                    color: Colors.white)), // Cambia el color del texto aquí
+          ),
+          DataColumn(
+            label: Text('PTS',
+                style: TextStyle(
+                    color: Colors.white)), // Cambia el color del texto aquí
+          ),
+          DataColumn(
+            label: Text('PJ',
+                style: TextStyle(
+                    color: Colors.white)), // Cambia el color del texto aquí
+          ),
+          DataColumn(
+            label: Text('PG',
+                style: TextStyle(
+                    color: Colors.white)), // Cambia el color del texto aquí
+          ),
+          DataColumn(
+            label: Text('PE',
+                style: TextStyle(
+                    color: Colors.white)), // Cambia el color del texto aquí
+          ),
+          DataColumn(
+            label: Text('PP',
+                style: TextStyle(
+                    color: Colors.white)), // Cambia el color del texto aquí
+          ),
+          DataColumn(
+            label: Text('GF',
+                style: TextStyle(
+                    color: Colors.white)), // Cambia el color del texto aquí
+          ),
+          DataColumn(
+            label: Text('GC',
+                style: TextStyle(
+                    color: Colors.white)), // Cambia el color del texto aquí
+          ),
+          DataColumn(
+            label: Text('DIF',
+                style: TextStyle(
+                    color: Colors.white)), // Cambia el color del texto aquí
+          ),
+        ],
+        rows: const [
+          DataRow(cells: [
+            DataCell(Text('Pythonicos')),
+            DataCell(Text('9')),
+            DataCell(Text('3')),
+            DataCell(Text('3')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('8')),
+            DataCell(Text('0')),
+            DataCell(Text('+8')),
+          ]),
+          DataRow(cells: [
+            DataCell(Text('Equipo 2')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+          ]),
+          DataRow(cells: [
+            DataCell(Text('Equipo 3')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+          ]),
+          DataRow(cells: [
+            DataCell(Text('Equipo 4')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+          ]),
+          DataRow(cells: [
+            DataCell(Text('Equipo 5')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+          ]),
+          DataRow(cells: [
+            DataCell(Text('Equipo 6')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+          ]),
+          DataRow(cells: [
+            DataCell(Text('Equipo 7')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+          ]),
+          DataRow(cells: [
+            DataCell(Text('Equipo 8')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+          ]),
+          DataRow(cells: [
+            DataCell(Text('Equipo 9')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+          ]),
+          DataRow(cells: [
+            DataCell(Text('Equipo 10')),
+            DataCell(Text('0')),
+            DataCell(Text('1')),
+            DataCell(Text('1')),
+            DataCell(Text('0')),
+            DataCell(Text('1')),
+            DataCell(Text('0')),
+            DataCell(Text('1')),
+            DataCell(Text('-1')),
+          ]),
+          DataRow(cells: [
+            DataCell(Text('Equipo 11')),
+            DataCell(Text('0')),
+            DataCell(Text('1')),
+            DataCell(Text('1')),
+            DataCell(Text('0')),
+            DataCell(Text('1')),
+            DataCell(Text('0')),
+            DataCell(Text('3')),
+            DataCell(Text('-3')),
+          ]),
+          DataRow(cells: [
+            DataCell(Text('Equipo 12')),
+            DataCell(Text('0')),
+            DataCell(Text('1')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('1')),
+            DataCell(Text('0')),
+            DataCell(Text('4')),
+            DataCell(Text('-4')),
+          ]),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_month),
+            label: 'Calendar',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.newspaper),
+            label: 'News',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.table_chart),
+            label: 'Table',
+          ),
+        ],
+      ),
+    );
+  }
 }
